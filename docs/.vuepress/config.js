2bb0740e9dba22a65ea02151efdfc59d6fe2bb72
Kim Hyunsu <khszone02@naver.com>
2018-07-15 19:00:13 +0900

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'Department of Physics',
+    description: 'Book Rental Service',
+    themeConfig: {
+        search: false
+    }
+}
---
+++ docs/.vuepress/theme/layouts/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app light>
+    <v-content>
+      <!-- https://medium.com/vue-mastery/how-to-create-a-custom-vuepress-theme-with-vuetify-651b7d7e5092 -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
